d4bb4caf82730e7ece9c9eb0b0ded76dee5495ab
Gangxin-Li <lgx.uofg@gmail.com>
2023-01-24 22:05:02 +0000

    Milestone4-Task1

--- milestone_4.py
+++ milestone_4.py
@@ -3,7 +3,7 @@ class Hangman():
     def __init__(self,word_list,num_lives=5) -> None:
         
         self.word = random.choice(word_list)
-        self.word_guessed = ['_','_','_','_','_']
+        self.word_guessed = ['_']*len(self.word)
         self.num_letters = 0
         self.num_lives = num_lives
         self.word_list = word_list
