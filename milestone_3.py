0857699b1699a9200f1988e2f723564f68d374cc
Gangxin-Li <lgx.uofg@gmail.com>
2023-01-24 19:58:25 +0000

    Milestone3-Task3

--- milestone_3.py
+++ milestone_3.py
@@ -1,21 +1,13 @@
 import random
 secret_list = ['apple','watermelon','fruits1','fruits2','fruits3']
 word = random.choice(secret_list)
-
-    
-
-def check_guess(guess):
-    guess = guess.lower()
+while True:
+    guess = input()
     if guess in word:
         print("Good guess! {guess} is in the word.".format(guess=guess))
-        return True
+        break
     else:
         print("Sorry, {guess} is not in the word. Try again.".format(guess=guess))
-        return False
-def ask_for_input():
-    guess = input()
-    if len(guess)==1 and guess.isalpha:
-        res = check_guess(guess)
-while True:
-    ask_for_input()
+
+
 
