bdc9111c361fbbf5563908c76771bf8f5b8a56e4
Gangxin-Li <lgx.uofg@gmail.com>
2023-01-24 21:28:02 +0000

    Milestone4-Task1

---
+++ milestone_4.py
@@ -0,0 +1,9 @@
+import random
+class Hangman():
+    def __init__(self,word_list,num_list=5) -> None:
+        self.word_list = ['apple','watermelon','fruits1','fruits2','fruits3']
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_','_','_','_','_']
+        self.num_letters = 0
+        self.num_lives = 0
+        self.list_of_guesses=[]
