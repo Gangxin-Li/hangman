d723c17a2c55f16e7b8903688d352a487cffacfa
Gangxin-Li <lgx.uofg@gmail.com>
2023-01-24 19:37:08 +0000

    Milestone3-Task2

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,14 @@
+import random
+secret_list = ['apple','watermelon','fruits1','fruits2','fruits3']
+secret = random.choice(secret_list)
 while True:
     guess = input()
-    if(len(guess)==1 and guess.isalpha):
+    if(guess in secret):
+        print("Good guess! {guess} is in the word.".format(guess=guess))
         break
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print("Sorry, {guess} is not in the word. Try again.".format(guess=guess))
+
+
+
+
