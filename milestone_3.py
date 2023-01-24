26466e6a4b803426f58b1278cffd964475fafe29
Gangxin-Li <lgx.uofg@gmail.com>
2023-01-24 19:53:55 +0000

    Milestone3-Task3

--- milestone_3.py
+++ milestone_3.py
@@ -8,11 +8,15 @@ def check_guess(guess):
     guess = guess.lower()
     if guess in word:
         print("Good guess! {guess} is in the word.".format(guess=guess))
+        return True
     else:
         print("Sorry, {guess} is not in the word. Try again.".format(guess=guess))
+        return False
 def ask_for_input():
     guess = input()
-    check_guess(guess)
+    res = check_guess(guess)
+    if res:
+        return
 while True:
     ask_for_input()
 
